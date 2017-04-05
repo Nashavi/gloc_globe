ada68801a337187465319c0ec15b4dcaaf9ac396
Nashavi <avinash.kamath@ucdenver.edu>
2017-04-05 01:04:41 -0700

    commit

---
+++ my loc.R
@@ -0,0 +1,49 @@
+setwd("~/Documents/UCD/BA Prac/Google loc/Takeout/Location History")
+library(jsonlite)
+require(dplyr)
+devtools::install_github("bwlewis/rthreejs")
+require(threejs)
+system.time(x <- fromJSON("LocationHistory.json"))
+system.time(y <- fromJSON("LocationHistory2.json"))
+
+loc = x$locations
+loc2 = y$locations
+
+# converting time column from posix milliseconds into a readable time scale
+loc$time = as.POSIXct(as.numeric(x$locations$timestampMs)/1000, origin = "1970-01-01")
+loc2$time = as.POSIXct(as.numeric(y$locations$timestampMs)/1000, origin = "1970-01-01")
+
+loc<- rbind(loc,loc2)
+
+# converting longitude and latitude from E7 to GPS coordinates
+loc$lat = loc$latitudeE7 / 1e7
+loc$lon = loc$longitudeE7 / 1e7
+
+
+ds<- loc[,c(9:11)]
+
+ds<- arrange(ds,time)
+
+ds<-ds[,c(2,3)]
+ds<-unique(ds)
+
+ds$lat<-round(ds$lat,2)
+ds$lon<-round(ds$lon,2)
+
+ds<-unique(ds)
+ds$laglat<- lag(ds$lat)
+ds$laglon<- lag(ds$lon)
+
+ds<-ds[-1,]
+
+
+save(ds,file="myloc2.RData")
+
+runApp(system.file("examples/globe", package="threejs"))
+earth <- system.file("images/world.jpg",  package="threejs")
+output$globe <- renderGlobe({ 
+  globejs(img=earth, lat=ds[,1], long=ds[,2], arcs=ds,
+        arcsHeight=0.3, arcsLwd=2, arcsColor="#ffff00", arcsOpacity=0.15, atmosphere=TRUE)
+})
+})
+
---
+++ mylocapp.R
@@ -0,0 +1,57 @@
+setwd("~/Documents/UCD/BA Prac/Google loc/Takeout/Location History")
+require(shiny)
+devtools::install_github("bwlewis/rthreejs")
+require(threejs)
+
+
+library("maptools")
+library("maps")
+data(wrld_simpl, package="maptools")
+
+
+shinyApp(
+  ui = fluidPage(
+    globeOutput("GlobeOut",width="100%",height="750px")
+    # absolutePanel(top=10,right=25, 
+    #               sliderInput("year", "Year:",min= 1991, max=2016, step=1 ,animate=animationOptions(loop= F,interval=500),value=1990,sep=""),
+    #               checkboxInput("legend", "Display legend", TRUE),
+    #               tags$div(class="header", checked=NA,
+    #                        tags$p("The Circle Markers indicate population increase"),tags$p("The Colors indicate the pop. growth in percentages"))
+    ),
+  server = function(input, output) {
+    load("myloc2.Rdata")
+    #earth <- system.file("images/world.jpg", package="threejs")
+    
+    bgcolor <- "#000025"
+    earth <- tempfile(fileext=".jpg")
+    jpeg(earth, width=2048, height=1024, quality=100, bg=bgcolor, antialias="default")
+    par(mar = c(0,0,0,0), pin = c(4,2), pty = "m", xaxs = "i",
+        xaxt = "n", xpd = FALSE, yaxs = "i", yaxt = "n")
+    
+    col <- rep("black",length(wrld_simpl$NAME))    # Set a default country color
+    
+    points = data.frame(lon=ds$lon, lat=ds$lat)
+    pointsSP = SpatialPoints(points, proj4string=CRS(proj4string(wrld_simpl)))  
+    indices = over(pointsSP, wrld_simpl)
+    ctry_list = unique(indices$NAME,na.rm=T)
+    
+    col[wrld_simpl$NAME %in% ctry_list] <- "#003344"    # Highlight countries visited
+    
+    
+    plot(wrld_simpl, col=col,          bg=bgcolor, border="#111111", ann=FALSE,  axes=FALSE, 
+         xpd=FALSE,  xlim=c(-180,180), ylim=c(-90,90),  setParUsrBB=TRUE)
+    
+    graphics.off()
+    
+    # bgcolor <- "#000025"
+    # earth <- tempfile(fileext=".jpg")
+    # jpeg(earth, width=2048, height=1024, quality=100, bg=bgcolor, antialias="default")
+    # par(mar = c(0,0,0,0), pin = c(4,2), pty = "m", xaxs = "i", xaxt = "n", xpd = FALSE, yaxs = "i", yaxt = "n")
+    
+    
+    output$GlobeOut <- renderGlobe({
+      args <- c(earth, list(lat=ds[,1], long=ds[,2], arcs=ds, val = .3,
+                            arcsHeight=0.3, arcsLwd=2, arcsColor="#ffff00", arcsOpacity=0.15, atmosphere=TRUE))
+      do.call(globejs, args=args)
+    })
+  })
